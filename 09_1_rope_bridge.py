7841af189bc89d47e8ac00f4ff443c2c61cbc0c2
tiago <tiagosf99@gmail.com>
2022-12-09 15:48:01 +0000

    Added solution to day 9 part 1.

---
+++ 09_1_rope_bridge.py
@@ -0,0 +1,38 @@
+with open("./data/input9.txt") as f:
+    instructions = f.read().splitlines()
+
+H_pos = [0, 0]
+T_pos = [0, 0]
+visited = set()
+
+
+def update_tail(head_pos, tail_pos):
+    if abs(head_pos[0] - tail_pos[0]) <= 1 and abs(head_pos[1] - tail_pos[1]) <= 1:
+        return
+    dif_x = head_pos[0] - tail_pos[0]
+    if dif_x:
+        tail_pos[0] += dif_x // abs(dif_x)
+    dif_y = head_pos[1] - tail_pos[1]
+    if dif_y:
+        tail_pos[1] += dif_y // abs(dif_y)
+
+
+for instruction in instructions:
+    direction, steps = instruction.split(" ")
+    steps = int(steps)
+
+    for step in range(steps):
+        if direction == "U":
+            H_pos[1] += 1
+        elif direction == "D":
+            H_pos[1] -= 1
+        elif direction == "L":
+            H_pos[0] -= 1
+        elif direction == "R":
+            H_pos[0] += 1
+
+        update_tail(H_pos, T_pos)
+
+        visited.add(tuple(T_pos))
+
+print(len(visited))
