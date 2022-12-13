724eb7ca91a1b8ad198553b40b973f54dbeafa30
tiago <tiagosf99@gmail.com>
2022-12-13 13:39:52 +0000

    Added comments.

--- 13_1_distress_signal.py
+++ 13_1_distress_signal.py
@@ -8,20 +8,27 @@ with open("./data/input13.txt") as f:
 def compare(left, right):
 
     for left_val, right_val in zip(left, right):
+        # Base case: compare two integers.
         if type(left_val) == int and type(right_val) == int:
             if left_val < right_val:
                 return True
             elif left_val > right_val:
                 return False
+            # Go to the next value.
             continue
+
+        # Cases where one of the values is not an int.
         elif not (type(left_val) == list and type(right_val) == list):
             if type(left_val) == int:
                 left_val = [left_val]
             else:
                 right_val = [right_val]
 
+        # Return if a decision has been made. Keep searching if still unclear.
         if (res := compare(left_val, right_val)) != None:
             return res
+
+    # Check cases where the comparison exhausted the pairs.
     if len(left) < len(right):
         return True
     elif len(left) > len(right):
@@ -31,6 +38,7 @@ def compare(left, right):
 index_sum = 0
 for i, pairs in enumerate(contents):
     left, right = pairs.split("\n")
+    # Convert strings to Python structures safely.
     left = ast.literal_eval(left)
     right = ast.literal_eval(right)
 
